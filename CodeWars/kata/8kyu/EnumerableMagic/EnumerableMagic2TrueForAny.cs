bac7b999fa85a2c6ee2cf3e59578ca624b510747
AbrahamOdamteng <abraham.odamteng.developer@gmail.com>
2017-12-04 18:11:13 +0000

    8kyu - EnumerableMagic2TrueForAny

--- CodeWars/kata/8kyu/EnumerableMagic/EnumerableMagic2TrueForAny.cs
+++ CodeWars/kata/8kyu/EnumerableMagic/EnumerableMagic2TrueForAny.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace CodeWars.kata._8kyu.EnumerableMagic
 {
+    /// <summary>
+    /// Create an any? (JS: any) function that accepts an array and a block (JS: function),
+    /// and returns true if the block (/function) returns true for any item in the array.
+    /// If the array is empty, the function should return false.
+    /// </summary>
     class EnumerableMagic2TrueForAny
     {
         public static bool Any(int[] arr, Func<int, bool> fun)
