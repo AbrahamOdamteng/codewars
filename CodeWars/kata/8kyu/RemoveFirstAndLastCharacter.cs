26672c6ab596a6c363866772c24811e551d4356c
unknown <abraham.odamteng.developer@gmail.com>
2017-12-04 13:58:19 +0000

    8kyu - RemoveFirstAndLastCharacter added description

--- CodeWars/kata/8kyu/RemoveFirstAndLastCharacter.cs
+++ CodeWars/kata/8kyu/RemoveFirstAndLastCharacter.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace CodeWars._8kyu
 {
+    /// <summary>
+    /// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+    /// You're given one parameter, except in C, where, to keep the difficulty at the level of the kata, you are given two parameters, 
+    /// the first a buffer with length exactly the same as the second parameter, the original string. 
+    /// You don't have to worry with strings with less than two characters.
+    /// </summary>
     class RemoveFirstAndLastCharacter
     {
         public static string Remove_char(string s)
